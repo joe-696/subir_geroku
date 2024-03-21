7e206a0e276e00a71fe2d70992fec71efe338521
jefferson joe mendoza vega <joe.mendoza.vega@gmail.com>
2024-03-20 20:13:54 -0500

    heroku ultimate234567

--- db/migrate/20240318223049_create_faculties.rb
+++ db/migrate/20240318223049_create_faculties.rb
@@ -1,9 +1,11 @@
 class CreateFaculties < ActiveRecord::Migration[7.1]
   def change
     create_table :faculties do |t|
-      t.string :name, unique: true
+      t.string :name
 
       t.timestamps
     end
+
+    add_index :faculties, :name, unique: true
   end
 end
