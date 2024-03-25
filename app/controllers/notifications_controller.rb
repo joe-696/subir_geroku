5566e5192bfae5d755ad862613e82510bf4c459c
jefferson joe mendoza vega <joe.mendoza.vega@gmail.com>
2024-03-25 01:28:40 -0500

    notificacion leida mejor

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,11 @@
 class NotificationsController < ApplicationController
     def index
+      mark_as_read = params[:mark_as_read] == 'true' # Verifica si se debe marcar como leídas
       @notifications = Current.user.notifications.order(created_at: :desc)
       @unread_notifications = Current.user.notifications.where(read: false)
-      @unread_notifications.update_all(read: true)
+      if mark_as_read
+        @unread_notifications.update_all(read: true) # Marca como leídas si es necesario
+      end
     end
   end
   
