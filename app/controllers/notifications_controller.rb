cd9f01ef942a7f076158b075efc61e5fdaa9ffed
jefferson joe mendoza vega <joe.mendoza.vega@gmail.com>
2024-03-25 01:05:44 -0500

    notificaciones arregladas?

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
     def index
       @notifications = Current.user.notifications.order(created_at: :desc)
       @unread_notifications = Current.user.notifications.where(read: false)
-      
+      @unread_notifications.update_all(read: true)
     end
   end
   
